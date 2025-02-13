d969b53bf4e4c36fc46623a13fe820a64b383a18
jdofat <143310236+jdofat@users.noreply.github.com>
2025-02-13 16:57:06 -0500

    Create getRandomTask.js

---
+++ getRandomTask.js
@@ -0,0 +1,38 @@
+*/ Task 3: Dynamic Content Using JSX and React Components (5 Points)
+
+Create an array of tasks inside the App component.
+Write a function getRandomTask() that returns a random task from the list.
+Display a random task inside a <h3> using JSX.
+
+/*
+
+function App () {
+  const tasks = [
+    "Read",
+    "Shop",
+    "Laundry"
+  ];
+
+  return (
+    <div>
+      {getRandomTask(tasks)}
+    </div>
+  );
+
+}
+
+function getRandomTask(tasks) {
+  const randomIndex = Math.floor(Math.random() * tasks.length);
+  const randomTask = tasks[randomIndex];
+
+  return (
+    <div> 
+      <h3> {randomTask} </h3>
+    </div>
+  );
+}
+
+
+
+
+
