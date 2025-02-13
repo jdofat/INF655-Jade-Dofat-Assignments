7d341d041695db165dd339e1807a7286393618ed
jdofat <143310236+jdofat@users.noreply.github.com>
2025-02-13 16:58:29 -0500

    Update Greeting.js

--- Greeting.js
+++ Greeting.js
@@ -1,11 +1,11 @@
-{*/ Task 1: Create a React Component Using JSX (5 Points)
+{/* Task 1: Create a React Component Using JSX (5 Points)
 Create a functional component called Greeting that:
 
 Uses JSX to return an <h1> element displaying a greeting message (e.g., "Hello, Welcome to React!").
 Includes a paragraph <p> with an inline style applied using JSX.
 Uses JavaScript expressions inside JSX (e.g., displaying the current date dynamically).
 
-/*}
+*/}
 
 function IsEven(number) {
   return number % 2 === 0;
