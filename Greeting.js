a0aa57e339a2b3d2a77ce39aedd92d0e4ea059b9
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-05 22:08:56 -0500

    Update Greeting.js

--- Greeting.js
+++ Greeting.js
@@ -1,9 +1,19 @@
-{/* Task 1: Create a React Component Using JSX (5 Points)
+{/* Week 1: Create a React Component Using JSX (5 Points)
 Create a functional component called Greeting that:
 
 Uses JSX to return an <h1> element displaying a greeting message (e.g., "Hello, Welcome to React!").
 Includes a paragraph <p> with an inline style applied using JSX.
 Uses JavaScript expressions inside JSX (e.g., displaying the current date dynamically).
+
+Week 2: Use Props to Pass Data (5 Points)
+\Modify your Greeting component to accept a prop called username.
+\Display the username inside the greeting message.
+In App.js, render Greeting twice with different usernames.
+
+Example Output:
+
+Hello, Alice!  
+Hello, Bob!  
 
 */}
 
@@ -13,12 +23,11 @@ function isEven(number) {
   return number % 2 === 0;
 };
 
-function Greeting() {
+function Greeting(props) {
   const number = 100;
-  
   return (
     <div>
-      <h1>Hello Assignment 1</h1>
+      <h1>Hello {props.username}</h1>
       <p style={{color: "red"}}>Is 100 an even number?: {isEven(number) ? "Yes" : "No"}</p>
     </div>
    );
