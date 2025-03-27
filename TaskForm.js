470f55b49d548a09f594f95a78e5e0f9b9a82f99
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-27 14:12:04 -0400

    Update TaskForm.js

--- TaskForm.js
+++ TaskForm.js
@@ -15,26 +15,25 @@ Add a "Sort by Name" button that sorts the tasks alphabetically when clicked.
 
 */}
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-function TaskForm() {
+function TaskForm({tasks, setTasks, deleteTask}) {
   const [taskName, setTaskName] = useState("");
   const [description, setDescription] = useState("");
-  const [tasks, setTasks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
-
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (taskName.trim() === "" || description.trim() === "") {
-      alert("enter task and description");
-      return;
+    
+      if (taskName.trim() === "" || description.trim() === "") {
+        alert("enter task and description");
+          return;
     }
-  };
   
     setTasks([...tasks, { taskName, description }]);
     setTaskName("");
     setDescription("");
-
+  };
+  
   const handleSort = () => {
     setTasks([...tasks].sort((a, b) => a.taskName.localeCompare(b.taskName)));
   };
@@ -49,7 +48,7 @@ function TaskForm() {
     <div>
      <form onSubmit={handleSubmit}>
         <label>
-         Add a Task:
+         Task:
            <input
              type="text"
              value={taskName}
