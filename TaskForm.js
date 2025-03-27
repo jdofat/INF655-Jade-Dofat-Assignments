a458a73056b233a5c28bb88d68f36ee55582316d
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-27 14:03:38 -0400

    Update TaskForm.js

--- TaskForm.js
+++ TaskForm.js
@@ -17,40 +17,57 @@ Add a "Sort by Name" button that sorts the tasks alphabetically when clicked.
 
 import { useState } from 'react';
 
-function taskForm() {
-  const [task, setTask] = useState("");
-  const [tasks,setTasks] = usestate([]);
-  const [searchTask, setSearch] = useState("");
+function TaskForm() {
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (task.trim() !== "") {
-      setTasks([...tasks, task]);
-      setTask("");
+    if (taskName.trim() === "" || description.trim() === "") {
+      alert("enter task and description");
+      return;
     }
   };
   
+    setTasks([...tasks, { taskName, description }]);
+    setTaskName("");
+    setDescription("");
+
   const handleSort = () => {
-    setTasks([...tasks].sort());
+    setTasks([...tasks].sort((a, b) => a.taskName.localeCompare(b.taskName)));
   };
-  
+
+  const filteredTasks = tasks.filter(task =>
+    task.taskName.toLowerCase().includes(searchTerm.toLowerCase()
+  };
+
+
+
   return (
     <div>
- `    <form onSubmit={handleSubmit}>
+     <form onSubmit={handleSubmit}>
         <label>
          Add a Task:
            <input
              type="text"
-             value={task}
-             onChange={(e) => setTask(e.target.value)
+             value={taskName}
+             onChange={(e) => setTaskName(e.target.value)
            }/>
-       </label>
-      <button type="submit">Submit</button>
-    </form>
+          </label>
+          
+  <label> Description:
+      <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+       />
+  </label>
+ <button type="submit">submit</button>
+</form>
 
     <input
       type="text"
-      placeholder="Search Task"
       value={searchTerm}
       onChange={(e) => setSearchTerm(e.target.value)
     }/>
@@ -58,12 +75,15 @@ function taskForm() {
     <button onClick={handleSort}> Sort by: Name </button>
 
     <ul>
-      {filteredatTask.map((task, index) => (
-        <li key={index}>{task}</li>
+      {filteredTasks.map((task, index) => (
+        <li key={index}>
+          {task.taskName}: 
+          {task.description}
+        </li>
       ))}
     </ul>
   </div>
-);
+ );
 }
 
 export default TaskForm; 
