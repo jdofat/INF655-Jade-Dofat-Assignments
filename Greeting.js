a4075a1683c06238c41fb65bbbb0dd7806d9aa80
jdofat <143310236+jdofat@users.noreply.github.com>
2025-03-27 12:25:10 -0400

    Update Greeting.js

--- Greeting.js
+++ Greeting.js
@@ -15,22 +15,34 @@ Example Output:
 Hello, Alice!  
 Hello, Bob!  
 
+modify this code to
+accept a username prop and display it inside the greeting message.
+Add a new paragraph that shows the current date using a JavaScript expression inside JSX.
+Add a button labeled "Change Greeting" that dynamically changes the greeting message when clicked
+
 */}
 
-import React from 'react';
+import React, { useState } from 'react';
 
 function isEven(number) {
   return number % 2 === 0;
 };
 
 function Greeting(props) {
+  const [myGreeting, setGreeting] = useState('Hello');
   const number = 100;
+  const myDate = new Date().toLocaleDateString();
+  const changeGreeting = () => {
+    setMyGreeting("Hi!");
+  };
+  
   return (
     <div>
-      <h1>Hello {props.username}</h1>
+      <h1>{myGreeting} {props.username}</h1>
       <p style={{color: "red"}}>Is 100 an even number?: {isEven(number) ? "Yes" : "No"}</p>
+      <button onClick={changeGreeting}>Change My Greeting</button>
     </div>
    );
-};
+}
 
 export default Greeting;
