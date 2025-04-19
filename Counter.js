72a28fceeeb401e11bf9b35dac0814c874134393
jdofat <143310236+jdofat@users.noreply.github.com>
2025-04-19 15:35:57 -0400

    Update Counter.js

--- Counter.js
+++ Counter.js
@@ -1,17 +1,3 @@
-{/*Week 2 Task 2:
-Manage State with useState (5 Points)
-Create a Counter component.
-Use the useState hook to track a count value.
-Display the count and a button to increase the count when clicked.
-Example Output:
-
-Count: 0  
-[Click Me]  
-(Clicking the button increases the count)
-
-export default App;
-*/}
-
 import { useState } from "react";
 
 function Counter() {
